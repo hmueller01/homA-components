360a8c272123770e288238666f497a99fa587303
Holger Mueller <holger@euhm.de>
2018-03-23 22:25:28 +0100

    changed config settings to uppercase

--- energy/sml_mqtt.py
+++ energy/sml_mqtt.py
@@ -11,12 +11,12 @@ import paho.mqtt.client as mqtt
 import mqtt_config		# defines secret host, port, user, pwd, ca_certs
 
 # config here ...
-debug = False
-systemId = "123456-energy"
-device = "Energy"
-room = "Sensors"
+DEBUG = False
+SYSTEMID = "123456-energy"
+DEVICE = "Energy"
+ROOM = "Sensors"
 # Reminder if HomA setup messages have been send, delete and restart to resend
-init_file = "/dev/shm/homa_init."+ systemId
+INIT_FILE = "/dev/shm/homa_init."+ SYSTEMID
 
 
 obis_arr = [
@@ -25,14 +25,14 @@ obis_arr = [
 
 def get_topic(t1 = None, t2 = None, t3 = None):
 	"Create topic string."
-	topic = "/devices/"+ systemId
+	topic = "/devices/"+ SYSTEMID
 	if t1:
 		topic += "/"+ t1
 	if t2:
 		topic += "/"+ t2
 	if t3:
 		topic += "/"+ t3
-	if debug: print("topic string = '"+ topic+ "'")
+	if DEBUG: print("topic string = '"+ topic+ "'")
 	return topic
 
 def scan_line(line):
@@ -43,7 +43,7 @@ def scan_line(line):
 			pos_start = line.find("#") + 1
 			pos_end = line.find("#", pos_start)
 			value = float(line[pos_start:pos_end])
-			if debug: print("Found obis topic '"+ obis_dict['topic']+ "' = line["+ str(pos_start)+ ":"+ str(pos_end)+ "] = '"+ str(value)+ "'")
+			if DEBUG: print("Found obis topic '"+ obis_dict['topic']+ "' = line["+ str(pos_start)+ ":"+ str(pos_end)+ "] = '"+ str(value)+ "'")
 			# scale value as specified
 			value = round(value / obis_dict['scale'], 1)
 			mqttc.publish(get_topic("controls", obis_dict['topic']), str(value), retain=True)
@@ -53,14 +53,14 @@ def scan_line(line):
 def homa_init():
 	"Publish HomA setup messages to MQTT broker."
 	# check if we need to init HomA
-	if os.path.isfile(init_file):
+	if os.path.isfile(INIT_FILE):
 		print("HomA setup data not reloaded, to do so delete "+ INIT_FILE+ " and restart.")
 		return
 	print("Publishing HomA setup data ...")
-	# set room name
-	mqttc.publish(get_topic("meta/room"), room, retain=True)
-	# set device name
-	mqttc.publish(get_topic("meta/name"), device, retain=True)
+	# set ROOM name
+	mqttc.publish(get_topic("meta/ROOM"), ROOM, retain=True)
+	# set DEVICE name
+	mqttc.publish(get_topic("meta/name"), DEVICE, retain=True)
 	# setup controls
 	order = 1
 	for obis_dict in obis_arr:
@@ -69,13 +69,13 @@ def homa_init():
 		mqttc.publish(get_topic("controls", obis_dict['topic'], "meta/order"), order, retain=True)
 		order += 1
 	# create init file
-	file = open(init_file, 'w')
+	file = open(INIT_FILE, 'w')
 	file.close()
 	return
 
 # The callback for when the client receives a CONNACK response from the broker.
 def on_connect(client, userdata, flags, rc):
-	if debug: print("Connected with result code "+ str(rc))
+	if DEBUG: print("Connected with result code "+ str(rc))
 
 	# Subscribing in on_connect() means that if we lose the connection and
 	# reconnect then subscriptions will be renewed.
@@ -84,12 +84,12 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the broker.
 def on_message(client, userdata, msg):
-	if debug: print(msg.topic+ " "+ str(msg.payload))
+	if DEBUG: print(msg.topic+ " "+ str(msg.payload))
 	return
 
 # The callback for when a message is published to the broker.
 def on_publish(client, userdata, mid):
-	if debug: print("message send "+ str(mid))
+	if DEBUG: print("message send "+ str(mid))
 	return
 
 
@@ -105,7 +105,7 @@ mqttc.username_pw_set(mqtt_config.user, password=mqtt_config.pwd)
 mqttc.connect(mqtt_config.host, port=mqtt_config.port)
 mqttc.loop_start()
 
-homa_init() # setup MQTT device and control settings
+homa_init() # setup MQTT DEVICE and control settings
 
 # read from stdin and scan for obis data
 print("Reading sml obis data from stdin forever. Press Ctrl-C to break.")
@@ -113,7 +113,7 @@ print("Reading sml obis data from stdin forever. Press Ctrl-C to break.")
 while True:
 	try:
 		line = sys.stdin.readline()
-		if debug: print(line)
+		if DEBUG: print(line)
 		scan_line(line)
 	except (KeyboardInterrupt, SystemExit):
 		print '\nKeyboardInterrupt found! Stopping program.'
