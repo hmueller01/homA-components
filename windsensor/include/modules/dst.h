8f88b025c6a4b391bc30e22cf0fa334534f14e79
Holger Müller <github@euhm.de>
2020-12-27 20:24:56 +0100

    c++ definition

--- windsensor/include/modules/dst.h
+++ windsensor/include/modules/dst.h
@@ -4,7 +4,16 @@
 #include <c_types.h>
 #include <time.h>
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 bool isDST(time_t timestamp);
 time_t applyDST(time_t timestamp);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 #endif // __DST_H__
