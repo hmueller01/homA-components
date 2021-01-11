87b05a1ca21759d35bb3febcd72ac42192fbbca5
Holger Müller <github@euhm.de>
2021-01-11 20:27:52 +0100

    added macros to cycle through lists and arrays

--- common/common.h
+++ common/common.h
@@ -40,7 +40,20 @@
 #define INFINITY __builtin_huge_valf()
 #endif
 
-#define strtolower(sptr) for (char *p = sptr; *p; ++p) *p = tolower(*p);
+#define strtolower(sptr) for (char *p = sptr; *p; ++p) *p = tolower(*p)
+
+// calculate size (number of elements) of an array
+#define ARRAYSIZE(array) ((int) (sizeof(array) / sizeof(*array)))
+
+// helper macro to cycle through a list in a for loop
+// see https://stackoverflow.com/questions/400951/does-c-have-a-foreach-loop-construct
+#define EACH_LIST(item, list) \
+	(__typeof(*(list)) *(item) = (list); (item); (item) = (item)->next)
+
+// helper macro to cycle through an array in a for loop
+#define EACH_ARRAY(item, array) \
+	(__typeof(*(array)) *p = (array), (item) = *p; p < &((array)[ARRAYSIZE(array)]); p++, (item) = *p)
+
 
 #ifdef __cplusplus
 extern "C" {
